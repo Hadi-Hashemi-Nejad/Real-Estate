0645f8404510663af43ff5b7d802a1c389e398a5
Hadi Hashemi <hadi.hashemi@uwalumni.com>
2024-03-18 13:52:06 +0400

    VPC edits

--- Terraform/VPC.tf
+++ Terraform/VPC.tf
@@ -20,6 +20,10 @@ resource "aws_subnet" "main_subnet" {
 #Creating an internet gateway to allow the VPC to connect to internet
 resource "aws_internet_gateway" "VPC_gateway" {
   vpc_id = aws_vpc.main_VPC.id
+
+    tags = {
+    Name = "internet door"
+  }
 }
 
 #Creating Custom Route Table that allows all ports access to internet
@@ -37,7 +41,7 @@ resource "aws_route_table" "route_table" {
   }
 
   tags = {
-    Name = "Lenient Rout Table"
+    Name = "Lenient Route Table"
   }
 }
 
@@ -45,4 +49,73 @@ resource "aws_route_table" "route_table" {
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main_subnet.id
   route_table_id = aws_route_table.route_table.id
+}
+
+#Creating a security group that allows all incoming and outgoing traffic
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_all_web_traffic"
+  description = "Allow Web inbound and outbound traffic"
+  vpc_id      = aws_vpc.main_VPC.id
+
+  ingress {
+    description = "All traffic into all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description = "All traffic out of all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all_traffic"
+  }
+}
+
+#Testing
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.main_subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_traffic.id]
+
+}
+# 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.VPC_gateway]
+}
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-0d7a109bf30624c99"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "main-key"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "web-server"
+  }
 }
